bbde3d57dc120e5a33faa7f4853da9ce8821f0c1
Roarkz <rory_bell_88@hotmail.com>
2020-01-09 16:22:30 +1000

    First front-end commit. basic routing for the Home, Register & login pages are complete.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Route exact path='/' component={Home} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+			</div>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
