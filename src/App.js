d94e39fa2447e777d2b092c4edd20f8e699487b7
Roarkz <rory_bell_88@hotmail.com>
2020-01-12 17:38:30 +1000

    A JWT token is being retuned from the back end and stored in the App components state. Now i will write a function to store it in session storage for authentication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import API from './axios'
 
 const App = () => {
+	const [tokenState, setTokenState] = useState(null)
+
+	const setToken = token => {
+		sessionStorage.setItem('token', token)
+		setTokenState(token)
+		// API.setHeader(token)
+	}
+
 	return (
 		<BrowserRouter>
 			<div>
 				<Route exact path='/' component={Home} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
+				<Route exact path='/register' component={Register} />
+				<Route
+					exact
+					path='/login'
+					render={props => {
+						return <Login {...props} setToken={setToken} />
+					}}
+				/>
 			</div>
 		</BrowserRouter>
 	)
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,28 +3,36 @@ import { Redirect } from 'react-router-dom'
 import RegLogForm from '../components/RegLogForm'
 import API from '../axios'
 
-const Login = () => {
+const Login = (props) => {
+
 	const [redirect, setRedirect] = useState(false)
 	const [flash, setFlash] = useState('')
+	const {setToken} = props
 
 	const handleLogin = event => {
 		event.preventDefault()
 		let username = document.getElementById('email-field')
 		let password = document.getElementById('password-field')
+
 		API.post('/login', {
 			username: `${username.value}`,
 			password: `${password.value}`
 		})
 			.then(res => {
 				if (res.status === 200) {
+					setToken(res.data.token)
 					setRedirect(true)
-				}
-				if (res.data.message) {
-					console.log(res.status)
 				}
 			})
 			.catch(err => {
-				console.log(err.response)
+				if (err.response && err.response.status === 401) {
+					setFlash(err.response.data)
+					username.value = ''
+					password.value = ''
+				}
+				else{
+					setFlash("sorry! something went wrong. Please try again later.")
+				}
 			})
 	}
 
@@ -36,7 +44,6 @@ const Login = () => {
 		<>
 			<h1>Login</h1>
 			{flash ? <p>{flash}</p> : null}
-
 			<RegLogForm link={'/register'} page='login' handleLogin={handleLogin} />
 		</>
 	)
